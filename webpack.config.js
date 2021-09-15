49b22b7ed9bd3a1f231075251e4f841d2bc108bc
Alex Bravo <alexander.bravo@intersystems.com>
2021-09-15 15:27:37 -0600

    typescript

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,32 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: path.join(__dirname, "src", "index.js"),
+    entry: "./src/index.tsx",
     output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
     mode: process.env.NODE_ENV || "development",
-    resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
     devServer: { static: path.join(__dirname, "src") },
     module: {
         rules: [
-            { 
-                test: /\.(js|jsx)$/, 
-                exclude: /node_modules/, 
-                use: ["babel-loader"] 
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: ["ts-loader"],
             },
             {
                 test: /\.(css|scss)$/,
                 use: ["style-loader", "css-loader"],
             },
-            { 
+            {
                 test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-                use: ["file-loader"] 
+                use: ["file-loader"],
             },
         ],
     },
